2de17fec000255b5128ebefd360126ba45f56a13
JOHN117JEFE <cristiandavidyacumal@gmailcom>
2025-03-27 11:15:04 -0500

    se agraga el proyecto adminsena

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\AreaController;
+use App\Http\Controllers\TrainingCenterController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ApprenticeController;
+use App\Http\Controllers\ComputerController;
+use App\Http\Controllers\CourseTeacherController;
+
+Route::resource('teachers', TeacherController::class);
+Route::resource('areas', AreaController::class);
+Route::resource('training-centers', TrainingCenterController::class);
+Route::resource('courses', CourseController::class);
+Route::resource('apprentices', ApprenticeController::class);
+Route::resource('computers', ComputerController::class);
+Route::resource('course-teacher', CourseTeacherController::class);
